d319be87be14ced51a839cb3a0163c2d8ba32497
Neco-ArcChaos <acceleratoor0123@gmail.com>
2026-07-29 16:29:16 +0800

    fix: enforce JVM 17 target for all Android subprojects

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -20,6 +20,22 @@ subprojects {
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
 
+subprojects {
+    afterEvaluate { project ->
+        if (project.plugins.hasPlugin("com.android.application") ||
+            project.plugins.hasPlugin("com.android.library")) {
+            project.extensions.findByType(com.android.build.gradle.BaseExtension::class.java)?.apply {
+                compileOptions {
+                    sourceCompatibility = JavaVersion.VERSION_17
+                    targetCompatibility = JavaVersion.VERSION_17
+                }
+                project.extensions.findByType(org.jetbrains.kotlin.gradle.dsl.KotlinAndroidProjectExtension::class.java)?.apply {
+                    jvmTarget = "17"
+                }
+            }
+        }
+    }
+}
 
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
